ac8ef5c3d0b234d161170427532f2cda4ad298c8
HOWOOK <hwshin1995@naver.com>
2018-07-07 19:55:40 +0900

    first commit

---
+++ app/src/main/java/com/example/q/a2ndweek/Tab1Contacts.java
@@ -0,0 +1,20 @@
+package com.example.q.a2ndweek;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab1Contacts extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tap1contacts, container, false);
+
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/example/q/a2ndweek/Tab2Gallery.java
@@ -0,0 +1,18 @@
+package com.example.q.a2ndweek;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Gallery extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tap2gallery, container, false);
+        return rootView;
+    }
+
+}

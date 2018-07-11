3800f4f5df89cf22b15013245072f23ed8df376f
HOWOOK <hwshin1995@naver.com>
2018-07-12 05:14:52 +0900

    1차완성

--- app/src/main/java/com/example/q/a2ndweek/MainActivity.java
+++ app/src/main/java/com/example/q/a2ndweek/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.q.a2ndweek;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,13 +9,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -101,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
                     Tab2Gallery tab2 = new Tab2Gallery();
                     return tab2;
                 case 2:
-                    Tab3Temp tab3 = new Tab3Temp();
+                    Tab3Chat tab3 = new Tab3Chat();
                     return tab3;
                     default:
                         return null;
